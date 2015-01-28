18e6828142a9dbebeecb3037bd3494c024007d8b
Brian Di Palma <offler@gmail.com>
2015-01-28 08:09:32 +0000

    Remove `rocambole-token` from package deps, it wasn't required.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var rocamboleToken = require('rocambole-token');
-
 module.exports.tokenBefore = function(token) {
 	if (token.type === 'LineComment') {
 		token.raw = '// ' + token.value;
