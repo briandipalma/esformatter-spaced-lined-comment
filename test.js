30b651184d75e73d60cd9243f7165bc2b3a3cef0
Brian Di Palma <offler@gmail.com>
2015-01-29 08:38:44 +0000

    Add a simple test

--- test.js
+++ test.js
@@ -1,9 +1,21 @@
 'use strict';
 
+var assert = require('assert');
+
 var mocha = require('mocha');
+var esformatter = require('esformatter');
+
+var spacedLinedCommentPlugin = require('./');
+
+esformatter.register(spacedLinedCommentPlugin);
 
 mocha.it('test', function() {
 	// Given.
+	var codeStr = '//Comment string';
+
 	// When.
+	var formattedCode = esformatter.format(codeStr);
+
 	// Then.
+	assert.equal(formattedCode, '// Comment string');
 });
